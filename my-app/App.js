a8d66ce55495c72c29c6b1beebfb7f8785554fab
dmfiz <119519694+dmfiz@users.noreply.github.com>
2023-12-25 19:33:38 +0000

    Create expo app

---
+++ my-app/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "navy",
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: "100%",
+    width: "100%",
+  },
+});
---
+++ my-app/screens/AuthScreen.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Organize yourself</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "navy",
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: "100%",
+    width: "100%",
+  },
+});
